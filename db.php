0860faf96a7d5731977056d96acfba6087181cec
elessa <elessamaefermano@gmail.com>
2023-05-27 10:23:23 +0800

    system

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+ class Database
+{
+    public $conn;
+    public $servername = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $dbname = "api_final";
+   
+    public function createDB()
+    {
+        $this->conn = new mysqli()
+    }
+}
+
+?>
