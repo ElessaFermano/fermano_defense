1572cf8a756f2504bbeb49cccb85cb24eb730999
elessa <elessamaefermano@gmail.com>
2023-05-27 10:58:52 +0800

    database created

--- classdb.php
+++ classdb.php
@@ -7,10 +7,12 @@ class Players extends Database
     public function createDB(): string 
     {
         $db = "CREATE DATABASE IF NOT EXISTS $this->dbname";
-        $this->conn->query($db);
+        return $this->conn->query($db);
     }
 }
 
 $d = new Players;
 $d->createDB();
+
+
 ?>
